3fe157a23e964d45f0c14b6a0405ebeeb0c9f661
pashkobohdan <pashkobohdan@gmail.com>
2018-07-21 13:37:05 +0300

    android 5- launch crash fix

--- app/src/main/java/com/pashkobohdan/ttsreader/TTSReaderApplication.kt
+++ app/src/main/java/com/pashkobohdan/ttsreader/TTSReaderApplication.kt
@@ -1,13 +1,14 @@
 package com.pashkobohdan.ttsreader
 
-import android.app.Application
-
+import android.content.Context
+import android.support.multidex.MultiDex
+import android.support.multidex.MultiDexApplication
 import com.pashkobohdan.ttsreader.di.ApplicationComponent
 import com.pashkobohdan.ttsreader.di.DaggerApplicationComponent
 import com.pashkobohdan.ttsreader.di.modules.AppModule
 import com.pashkobohdan.ttsreader.di.modules.DatabaseModule
 
-open class TTSReaderApplication : Application() {
+open class TTSReaderApplication : MultiDexApplication() {
     protected var component: ApplicationComponent? = null
 
     open fun getApplicationComponent(): ApplicationComponent {
@@ -22,6 +23,11 @@ open class TTSReaderApplication : Application() {
         } else {
             return currentComponent
         }
+    }
+
+    override fun attachBaseContext(context: Context) {
+        super.attachBaseContext(context)
+        MultiDex.install(this)
     }
 
     override fun onCreate() {
